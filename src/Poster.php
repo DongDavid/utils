33bfb447176abe7aaaa112024161f5357aadaadb
DongDavid <DongDavid@users.noreply.github.com>
2020-09-04 08:18:34 +0000

    Apply fixes from StyleCI

--- src/Poster.php
+++ src/Poster.php
@@ -612,7 +612,7 @@ class Poster
         $qrCode->setErrorCorrectionLevel(\Endroid\QrCode\ErrorCorrectionLevel::HIGH());
         $qrCode->setSize(330);
         $qrCode->setMargin(10);
-        if( !empty($logo_path) ){
+        if (!empty($logo_path)) {
             $qrCode->setLogoPath($logo_path);
             $qrCode->setLogoSize(100, 100);
         }
